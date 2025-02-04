5602738c8752209e6dc2bb91afae6ca865098ba1
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-02-04 23:10:30 +0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <>
+          <section className="pink_container">
+            <h1 className="heading">Pitch Your Startup,<br/>
+                Connect with Entrepreneurs
+            </h1>
+            <p className="sub-heading !max-w-3xl">
+                Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+            </p>
+
+          </section>
+      </>
+  );
+}
