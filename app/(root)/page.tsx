0d9aa0d3ddadca366a06919667fbef2988d19d16
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-02-05 10:29:00 +0800

    Add startup card component and improve homepage layout
    Introduced the `StartupCard` component and integrated it into the homepage to display a list of startups with details like author, views, and creation date. Added a utility function `formatDate` for date formatting and updated the Next.js config to allow remote image loading. These changes enhance the UI/UX and provide a foundation for startup-related features.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {
     searchParams: Promise<{ query?: string }>
 }) {
     const query = (await searchParams).query;
+
+    const posts = [{
+        _createdAt: new Date(),
+        views: 55,
+        author: { _id: 1, name: 'Mark'},
+        _id: 1,
+        description: 'This is a description',
+        image: 'https://images.unsplash.com/photo-1563207153-f403bf289096?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cm9ib3RzfGVufDB8fDB8fHww',
+        category: 'Robots',
+        title: 'We Robots',
+    }];
 
   return (
       <>
@@ -17,6 +29,23 @@ export default async function Home({searchParams}: {
               </p>
 
               <SearchForm query={query}/>
+          </section>
+
+          <section className="section_container ">
+              <p className="text-30-semibold">
+                  {query ? `Search results for "${query}"` : 'All Startups'}
+              </p>
+
+              <ul className="mt-7 card_grid">
+                  {posts?.length > 0 ? (
+                      posts.map(( post:StartupCardType) => (
+                          <StartupCard key={post?._id} post={post}/>
+                      ))
+                  ):(
+                      <p className="no-resuls">No startups found</p>
+                  )}
+
+              </ul>
           </section>
       </>
   );
