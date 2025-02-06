0d24b693a89b5a21b1860195fa8374fb952a157c
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-02-06 17:39:00 +0800

    Implement GitHub-based authentication with user provisioning
    Added GitHub authentication via NextAuth, including user creation in Sanity when missing. Extended session and JWT objects to include user IDs, ensuring seamless integration with existing systems.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,6 +2,7 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 import {sanityFetch, SanityLive} from "@/sanity/lib/live";
+import {auth} from "@/auth";
 
 export default async function Home({searchParams}: {
     searchParams: Promise<{ query?: string }>
@@ -9,6 +10,10 @@ export default async function Home({searchParams}: {
     const query = (await searchParams).query;
     const params = {search: query || null};
     const {data: posts} = await sanityFetch({query: STARTUPS_QUERY, params})
+
+    const session = await auth();
+
+    console.log(session?.id);
 
   return (
       <>
